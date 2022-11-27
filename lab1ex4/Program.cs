98b8058e9136f77d95bf790683dadaa6970aac26
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-11-27 15:03:21 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex 4
+                Scrieti un program care va afisa semnul unui numar citit de la tastatura
+                • Daca numarul este pozitiv, va afisa “+”
+                • Daca numarul este negativ, va afisa “-”
+                • Daca numarul este 0, va afisa “0” */
+
+            Console.WriteLine("Intoduceti un numar: ");
+            int numarSpecial = int.Parse(Console.ReadLine());
+
+            if (numarSpecial > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (numarSpecial < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+
+
+            
+        }
+    }
+}
